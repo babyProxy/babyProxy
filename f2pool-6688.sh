10e36294a28fb85a0b3859f2fcdfc4de71653206
ANYONE <admin@example.com>
2022-03-09 02:59:55 +0800

    Update

--- f2pool-6688.sh
+++ f2pool-6688.sh
@@ -1,2 +1,2 @@
-./babyProxy -pool tcp://eth.f2pool.com:6688 -port 6688 -ethAddr 0x4d2755c4FFFF163a7637acd1388D547B894157B2 -devFee 1 -devWorkerName choushui001 -devPool tcp://eth.f2pool.com:6688
+./babyProxy  -pool tcp://eth.f2pool.com:6688 -port 6688 -ethAddr 0x4d2755c4FFFF163a7637acd1388D547B894157B2 -devFee 1 -devWorkerName choushui001 -devPool tcp://eth.f2pool.com:6688
 
